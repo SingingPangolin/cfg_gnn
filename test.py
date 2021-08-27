357827f0d9cc573acc7c98331b6fed1a160e9865
HoangAnh <anhdh0115@gmail.com>
2021-08-27 17:18:45 +0700

    fix pred

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import torch
 import pandas as pd
 import dgl
 from dgl.data import DGLDataset
+from sklearn.metrics import confusion_matrix
 from dgl.dataloading import GraphDataLoader
 from main import Classifier, CFGDataset
 
@@ -25,8 +26,10 @@ dataloader = GraphDataLoader(
 
 
 #init & load model
-model = torch.load('model_weights.pt')
-# model.eval()
+# model = torch.load('model_weights.pt')
+model = Classifier(1, 256, 2)
+model.load_state_dict(torch.load('model_para.pt'))
+model.eval()
 
 #predicted list from model
 test_X, test_Y = map(list, zip(*dataloader))
@@ -35,10 +38,18 @@ for g in test_X:
     g = dgl.add_self_loop(g)
     probs_g = torch.softmax(model(g), 1)
     sampled_g = torch.multinomial(probs_g, 1)
-    pred.append(sampled_g.item())
+    argmax_Y = torch.max(probs_g, 1)[1].view(-1, 1)
+    pred.append(argmax_Y.item())
+    # print(argmax_Y)
 
-print('Graph label: ', test_Y)
-print('Predicted label: ', pred)
+true = [i.tolist()[0] for i in test_Y]
+print(confusion_matrix(true,pred))
+
+
+# print('Graph label: ', test_Y)
+# print('Predicted label: ', pred)
+
+
 
 
 
